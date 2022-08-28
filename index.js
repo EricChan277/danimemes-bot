b7f35fa824eac45cf408cf812006e4260d32d3f8
EricChan277 <ericchan277@gmail.com>
2022-08-27 23:39:47 -0700

    Added DarkMode

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 function DanButton(url) {
     window.open(url, '_blank').focus
 }
+function DarkMode(){
+    const dBody =  document.getElementById('dModeBody')
+    const g = 'body2';
+    const x = 'body';
+
+    if (dBody.classList.contains(g)) {
+        document.getElementById('dModeBody').classList.remove('body2');
+        document.getElementById('dModeBody').classList.add('body');
+    }
+    else {
+        document.getElementById('dModeBody').classList.remove('body');
+        document.getElementById('dModeBody').classList.add('body2');
+    }
+  document.getElementById('descText1').classList.toggle('text');
+  document.getElementById('descText2').classList.toggle('text');
+  document.getElementById('descText3').classList.toggle('text');
+}
